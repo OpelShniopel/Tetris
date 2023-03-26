19e5d97e83ea779abcc608ca3a1de12ca7fd9ec0
MartKarp <111006315+MartKarp@users.noreply.github.com>
2023-03-26 13:00:02 +0300

    Fixed music so it doesn't restart after switching scenes

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [Header("----------- Garso šaltinis ------------")]
+    [SerializeField] AudioSource musicSource;
+
+    [Header("----------- Garso įrašas ------------")]
+    public AudioClip backgroundMusic;
+
+    public static AudioManager instance;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
