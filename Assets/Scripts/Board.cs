a142a3a7885c94fb5c24e8c9875509f99b690909
OpelShniopel <112033023+OpelShniopel@users.noreply.github.com>
2023-04-01 19:26:45 +0300

    Added ClearBoard() method.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -222,76 +222,52 @@ public class Board : MonoBehaviour
         }
     }
     
+    public void ClearBoard()
+    {
+        RectInt bounds = Bounds;
+    
+        for (int row = bounds.yMin; row < bounds.yMax; row++)
+        {
+            for (int col = bounds.xMin; col < bounds.xMax; col++)
+            {
+                Vector3Int position = new Vector3Int(col, row, 0);
+                Tilemap.SetTile(position, null);
+            }
+        }
+    }
+
     public bool CheckGameOver()
     {
         RectInt boardBounds = Bounds;   // Get the boundaries of the board.
         int spawnRow = SpawnPosition.y; // Get the row index where the pieces spawn.
-        bool isGameOver = false;        // Flag to determine if the game is over or not.
-        
-        // Iterate through each column in the spawn row.
 
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
 
+        bool isEndlessTetris = sceneName == "EndlessTetris";
 
-
-        if (sceneName == "EndlessTetris")
-        {
-            isGameOver = false;
-            // Iterate through each column in the spawn row.
-            for (int columnIndex = boardBounds.xMin; columnIndex <= boardBounds.xMax; columnIndex++)
-            {
-                // Get the tile position at the spawn row and current column.
-                Vector3Int tilePosition = new Vector3Int(columnIndex, spawnRow, 0);
-
-
-                // If there's no tile at the tile position, skip to the next iteration.
-                if (!Tilemap.HasTile(tilePosition)) continue;
-                else
-                {
-                    RectInt bounds = Bounds;
-                    for (int row = bounds.yMin; row <= bounds.yMax; row++) 
-                    {
-                        LineClear(row);
-                        
-                    }
-                }             
-            }
-            
-        }
-        //if (sceneName == "Tetris")
         for (int columnIndex = boardBounds.xMin; columnIndex < boardBounds.xMax; columnIndex++)
         {
             // Get the tile position at the spawn row and current column.
             Vector3Int tilePosition = new Vector3Int(columnIndex, spawnRow, 0);
 
-
             // If there's no tile at the tile position, skip to the next iteration.
             if (!Tilemap.HasTile(tilePosition)) continue;
-            GameOver(); // Call the GameOver method to end the game.
 
-            isGameOver = true; // Set the isGameOver flag to true since a tile is present in the spawn row.
-            break;
+            if (isEndlessTetris)
+            {
+                ClearBoard();
+            }
+            else
+            {
+                GameOver();
+                return true; // Game over since a tile is present in the spawn row.
+            }
         }
 
-        //if (isGameOver && heartSystem.Length > 0)
-        //{
-        //    heartSystem = new HeartSystem[heartSystem.Length - 1];
-        //    for (int i = 0; i < heartSystem.Length; i++)
-        //    {
-        //        heartSystem[i] = new HeartSystem(this);
-        //    }
-        //    return false;
-            
-
-        //}
-
-        
-
-
-        return isGameOver; // Return the value of isGameOver.
+        return false; // No game over.
     }
-    
+
     private void GameOver()
     {
         // Load the Game Over scene
