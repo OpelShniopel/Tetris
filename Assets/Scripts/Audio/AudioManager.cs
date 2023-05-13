3cc0247e1bdf76917da4f0ddd571826c6733abec
OpelShniopel <112033023+OpelShniopel@users.noreply.github.com>
2023-05-13 13:30:19 +0300

    Added namespaces.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,46 +1,45 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Tetris.Audio
 {
-    
-    public class AudioManager : MonoBehaviour
+    public sealed class AudioManager : MonoBehaviour
     {
-     /// <summary>
-     /// The clip to play outside menus.
-     /// </summary>
-        [SerializeField]
-        private AudioClip levelMusic;
- 
-        [SerializeField]
-     /// <summary>
-     /// The component that plays the music
-     /// </summary>
-        private AudioSource source;
- 
-     /// <summary>
-     /// This class follows the singleton pattern and this is its instance
-     /// </summary>
-        static private AudioManager instance;
- 
-     /// <summary>
-     /// Awake is not public because other scripts have no reason to call it directly,
-     /// only the Unity runtime does (and it can call protected and private methods).
-     /// It is protected virtual so that possible subclasses may perform more specific
-     /// tasks in their own Awake and still call this base method (It's like constructors
-     /// in object-oriented languages but compatible with Unity's component-based stuff.
-     /// </summary>
-        protected virtual void Awake() {
-         // Singleton enforcement
-            if (instance == null) {
-             // Register as singleton if first
-                instance = this;
-                DontDestroyOnLoad(this);
-            } else if(instance != this)
+        /// <summary>
+        ///     The clip to play outside menus.
+        /// </summary>
+        [SerializeField] private AudioClip levelMusic;
+
+        /// <summary>
+        ///     The component that plays the music
+        /// </summary>
+        [SerializeField] private AudioSource source;
+
+        /// <summary>
+        ///     This class follows the singleton pattern and this is its instance
+        /// </summary>
+        private static AudioManager Instance { get; set; }
+
+        /// <summary>
+        ///     Awake is not public because other scripts have no reason to call it directly,
+        ///     only the Unity runtime does (and it can call protected and private methods).
+        ///     It is protected virtual so that possible subclasses may perform more specific
+        ///     tasks in their own Awake and still call this base method (It's like constructors
+        ///     in object-oriented languages but compatible with Unity's component-based stuff.
+        /// </summary>
+        private void Awake()
+        {
+            // Singleton enforcement
+            if (Instance == null)
             {
-                instance.source.Stop();
-                instance = this;
-                Destroy(instance);
+                // Register as singleton if first
+                Instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else if (Instance != this)
+            {
+                Instance.source.Stop();
+                Instance = this;
+                Destroy(Instance);
                 PlayGameMusic();
             }
         }
@@ -49,19 +48,18 @@ namespace Tetris.Audio
         //{
         //    PlayGameMusic();
         //}
-     
-     /// <summary>
-     /// Plays the music designed for outside menus
-     /// This method is static so that it can be called from anywhere in the code.
-     /// </summary>
-        static public void PlayGameMusic ()
+
+        /// <summary>
+        ///     Plays the music designed for outside menus
+        ///     This method is static so that it can be called from anywhere in the code.
+        /// </summary>
+        private static void PlayGameMusic()
         {
-            if (instance != null) {
-                if (instance.source != null) {
-                        instance.source.Stop();
-                        instance.source.clip = instance.levelMusic;
-                        instance.source.Play();
-                }
+            if (Instance != null && Instance.source != null)
+            {
+                Instance.source.Stop();
+                Instance.source.clip = Instance.levelMusic;
+                Instance.source.Play();
             }
         }
     }
